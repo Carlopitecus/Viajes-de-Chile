dd58f1280d777b04a71c1f1dbac1804e95f473f2
Carlopitecus <carlosefariasl@gmail.com>
2021-06-02 21:55:19 -0400

    Se corrige el smooth scroll, y se agrega un tooltip en las redes en el footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     var qs, js, q, s, d = document, gi = d.getElementById, ce = d.createElement, gt = d.getElementsByTagName, id = "typef_orm", b = "https://embed.typeform.com/"; if (!gi.call(d, id)) { js = ce.call(d, "script"); js.id = id; js.src = b + "embed.js"; q = gt.call(d, "script")[0]; q.parentNode.insertBefore(js, q) }
 
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
 
+      
 })
